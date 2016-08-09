c2d75dfbf2197ea99d61229952a9ca1d2237c9fa
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-09 08:42:17 +0300

    ejs -> pug, create index page with random number

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.get('/', function(request, response) {
-  response.render('pages/index')
+    console.time('render');
+    response.render('pages/index', {
+    	number: Math.random()*100
+    });
+    console.timeEnd('render');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
