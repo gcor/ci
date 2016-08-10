b92f6343a31fde8ab8fc116c3704a3609aaa5de2
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-10 18:50:00 +0300

    add mocha in .eslint, fix tests

--- app/random.js
+++ app/random.js
@@ -1,7 +1,6 @@
-function getRandomWeather() {
-	return Math.round(Math.random() * 100 - 50)
-}
-
-module.exports = {
-	getRandomWeather: getRandomWeather
-}
+/* Получаем рандомное число от -50 до +50 */
+module.exports.getRandomWeather = function () {
+	const weather = (Math.random() * 100) - 50;
+	const roundRandom = weather;
+	return Math.round(roundRandom);
+};
