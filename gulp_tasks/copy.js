4710aea45d0999181a935fe58310a034f5f89202
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-10 10:06:13 +0300

    fix gulp & eslint

--- gulp_tasks/copy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(gulp, $, browserSync, config) {
-    return function() {
-        gulp.src(config.from)
-            .pipe(gulp.dest(config.to))
-            .pipe(browserSync.reload({stream: true}));
-    }
-}
--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,149 @@
-var path = require("path");
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+const gulp = require('gulp'),
+    less = require('gulp-less'),
+    csso = require('gulp-csso'),
+    uglify = require('gulp-uglify'),
+    webpack = require('gulp-webpack'),
+    imagemin = require('gulp-imagemin'),
+    plumber = require('gulp-plumber'),
+    sourcemaps = require('gulp-sourcemaps'),
+    prefixer = require('gulp-autoprefixer'),
+    bsync = require('browser-sync');
 
-const buildPath = './public/'; 
-const srcPath = './src/'; 
+const browserSync = bsync.create(),
+    reload = browserSync.reload,
+    buildPath = './public/',
+    srcPath = './src/',
+    config = {
+        client: {
+            port: 8080,
+            files: 'src/**/*',
+            syncServer: buildPath
+        },
+        from: {
+            lessCommon: srcPath + 'styles/style.less',
+            less: srcPath + 'styles/**/*.less',
+            js: [srcPath + 'js/**/*.js', srcPath + 'templates/**/*'],
+            img: srcPath + 'img/**/*',
+            media: srcPath + 'media/**/*'
+        },
+        to: {
+            css: buildPath + 'css/',
+            js: buildPath + 'js/',
+            img: buildPath + 'img/',
+            media: buildPath + 'media/**/*'
+        }
+    };
 
-function getTask(task, config) {
-    return require('./gulp_tasks/' + task)(gulp, plugins, browserSync, config);
-}
-
-const config = {
-    server: {
-        files: ["views/**/*", "index.js"],
-        expressPort: 5000,
-        port: 7000,
-        main: "index.js"
-    },
-    client: {
-        port: 8080,
-        files: "src/**/*",
-        syncServer: buildPath
-    },
-    from: {
-        less_common: srcPath +"styles/style.less",
-        less: srcPath +"styles/**/*.less",
-        js: [srcPath +"js/**/*.js", srcPath +"templates/**/*"],
-        img: srcPath +"img/**/*",
-        media: srcPath +"media/**/*",
-    },
-    to: {
-        css: buildPath + "css/",
-        js: buildPath + "js/",
-        img: buildPath + "img/",
-        media: buildPath + "media/**/*"
-    }
-};
 
 gulp
-    .task('js', getTask('js', {
-        from: config.from.js,
-        to: config.to.js
-    }))
-    .task('less', getTask('less', {
-        from: config.from.less_common,
-        to: config.to.css
-    }))
-    .task('images', getTask('img', {
-        from: config.from.img,
-        to: config.to.img
-    }))
-    .task('media', getTask('copy', {
-        from: config.from.media,
-        to: config.to.media
-    }))
-    .task('client-sync', getTask('sync', {
-        open: false,
-        server: config.client.syncServer,
-        files: config.client.files,
-        port: config.client.port
-    }));
+    .task('client-sync', function() {
+        browserSync.init(null, {
+            notify: false,
+            server: config.client.server,
+            files: config.client.files,
+            port: config.client.port
+        });
+    })
+
+
+.task('less', function() {
+        gulp.src(config.from.lessCommon)
+            .pipe(plumber())
+            .pipe(sourcemaps.init())
+            .pipe(less())
+            .on('error', console.log)
+            .pipe(prefixer())
+            .pipe(csso())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(config.to.css))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+    })
+    .task('js', function() {
+        gulp.src(config.from.js)
+            .pipe(webpack({
+                module: {
+                    loaders: [{
+                        test: /\.jsx?$/,
+                        loader: 'babel?presets[]=es2015'
+                    }, {
+                        test: /\.hbs$/,
+                        loader: 'handlebars-loader'
+                    }]
+                },
+                output: {
+                    filename: 'app.js'
+                }
+            }))
+            .pipe(uglify())
+            .pipe(gulp.dest(config.to.js))
+            .pipe(reload({
+                stream: true
+            }));
+    })
+    .task('media', function() {
+        gulp.src(config.from.media)
+            .pipe(gulp.dest(config.to.media))
+            .pipe(reload({
+                stream: true
+            }));
+    })
+    .task('images', function() {
+        gulp.src(config.from.img)
+            .pipe(imagemin({
+                progressive: true,
+                svgoPlugins: [{
+                    removeViewBox: false
+                }],
+                interlaced: true
+            }))
+            .pipe(gulp.dest(config.to.img))
+            .pipe(reload({
+                stream: true
+            }));
+    });
 
 gulp
-    .task('build', getTask('build', {
-        fromJS: config.from.js,
-        toJS: config.to.js,
-        fromCSS: config.from.less_common,
-        toCSS: config.to.css,
-        fromImg: config.from.img,
-        toImg: config.to.img,
-        fromMedia: config.from.media,
-        toMedia: config.to.media
-    }))
+    .task('build', function() {
+        gulp.src(config.from.lessCommon)
+            .pipe(less())
+            .pipe(prefixer())
+            .pipe(csso())
+            .pipe(gulp.dest(config.to.css));
+
+        gulp.src(config.from.js)
+            .pipe(webpack({
+                module: {
+                    loaders: [{
+                        test: /\.jsx?$/,
+                        loader: 'babel?presets[]=es2015'
+                    }, {
+                        test: /\.hbs$/,
+                        loader: 'handlebars-loader'
+                    }]
+                },
+                output: {
+                    filename: 'app.js'
+                }
+            }))
+            .pipe(uglify())
+            .pipe(gulp.dest(config.to.js));
+
+        gulp.src(config.from.media)
+            .pipe(gulp.dest(config.to.media));
+
+        gulp.src(config.from.img)
+            .pipe(imagemin({
+                progressive: true,
+                svgoPlugins: [{
+                    removeViewBox: false
+                }],
+                interlaced: true
+            }))
+            .pipe(gulp.dest(config.to.img));
+    })
     .task('w', ['less', 'js', 'images', 'client-sync'], () => {
-        gulp.watch(config.from.pug, ['pug']);
         gulp.watch(config.from.less, ['less']);
         gulp.watch(config.from.js, ['js']);
         gulp.watch(config.from.img, ['images']);
