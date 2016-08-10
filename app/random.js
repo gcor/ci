f510671f1fd83d9162603b7d4adc9b9d5be29f55
Anton Ahatov <ahatov@gismeteo.ru>
2016-08-10 19:00:20 +0300

    add .eslintignore

--- app/random.js
+++ app/random.js
@@ -1,6 +1,6 @@
 /* Получаем рандомное число от -50 до +50 */
 module.exports.getRandomWeather = function () {
 	const weather = (Math.random() * 100) - 50;
-	const roundRandom = weather;
-	return Math.round(roundRandom);
+
+	return Math.round(weather);
 };
